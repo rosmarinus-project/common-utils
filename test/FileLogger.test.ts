eeee4e789f6dfb924529c26867713c806f215e63
JOJO <978940343@qq.com>
2023-11-09 11:04:30 +0800

    fix: test:unit

--- test/FileLogger.test.ts
+++ test/FileLogger.test.ts
@@ -6,19 +6,19 @@ describe('FileLogger', () => {
   afterEach(() => {});
 
   test('log meta', () => {
-    const logger = initFileLoggerFactory({ isProduction: false }).defaultLogger;
+    const logger = initFileLoggerFactory({ fileMode: 'console' }).defaultLogger;
 
     logger.info('log meta', { a: 1, b: 2 });
   });
 
   test('log msg', () => {
-    const logger = initFileLoggerFactory({ isProduction: false }).defaultLogger;
+    const logger = initFileLoggerFactory({ fileMode: 'console' }).defaultLogger;
 
     logger.info('log msg');
   });
 
   test('log not obj meta', () => {
-    const logger = initFileLoggerFactory({ isProduction: false }).defaultLogger;
+    const logger = initFileLoggerFactory({ fileMode: 'console' }).defaultLogger;
 
     logger.info('log not obj meta', 1, 'meta');
   });
