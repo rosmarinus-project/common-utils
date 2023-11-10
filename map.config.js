f306b332eeabfc3c1e58d0e2361680bc34b5edd4
JOJO <978940343@qq.com>
2023-11-10 19:23:46 +0800

    fix: cwd

--- map.config.js
+++ map.config.js
@@ -2,6 +2,5 @@ module.exports = {
   input: ['src/functions/*.ts', 'src/modules/*.ts'],
   exclude: ['src/functions/index.ts', 'src/modules/index.ts'],
   outputFileName: 'dist/map.json',
-  cwd: '/Users/joestarhu/Documents/programming/github/common-utils',
   srcDir: 'src',
 };
