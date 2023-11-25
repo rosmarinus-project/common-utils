d642827cfe83a1f602cb5dd25e55342c5d2c36a6
JOJO <978940343@qq.com>
2023-11-25 16:20:36 +0800

    feat: add promise timeout

--- src/functions/sleep.ts
+++ src/functions/sleep.ts
@@ -13,3 +13,25 @@ export const requestIdle = (timeout = 1000) => new Promise((resolve) => requestI
  * 超时
  */
 export const timeout = (ms = 0) => new Promise((reject) => setTimeout(reject, ms));
+
+export function wrapTimeoutForPromiseReject<T>(promise: Promise<T>, timeout = 3000): Promise<T> {
+  return Promise.race([
+    promise,
+    new Promise<T>((_, reject) => {
+      setTimeout(() => {
+        reject(`promise timeout: ${timeout}ms`);
+      }, timeout);
+    }),
+  ]);
+}
+
+export function wrapTimeoutForPromise<T>(promise: Promise<T>, timeout = 3000): Promise<T | undefined> {
+  return Promise.race([
+    promise,
+    new Promise<undefined>((resolve) => {
+      setTimeout(() => {
+        resolve(undefined);
+      }, timeout);
+    }),
+  ]);
+}
