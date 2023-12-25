5035445edcfff0e013422c5850ce8d8bc218d106
JOJO <978940343@qq.com>
2023-12-25 21:33:48 +0800

    fix: hookAsyncData support before and after hook

--- src/functions/hooks.ts
+++ src/functions/hooks.ts
@@ -1,6 +1,11 @@
 import { isPromise } from './is-type';
 
-export function hookAsyncData<T>(init: Promise<T> | (() => Promise<T>)) {
+export interface HookAsyncDataOptions<T> {
+  beforeUpdate?: (data: T) => void | Promise<void>;
+  afterUpdate?: (data: T) => void | Promise<void>;
+}
+
+export function hookAsyncData<T>(init: Promise<T> | (() => Promise<T>), options?: HookAsyncDataOptions<T>) {
   const initPromise = isPromise(init) ? init : init();
   let data: T | undefined = undefined;
 
@@ -15,12 +20,18 @@ export function hookAsyncData<T>(init: Promise<T> | (() => Promise<T>)) {
   return {
     getData,
     setData: (newData: T) => {
+      options?.beforeUpdate?.(newData);
       data = newData;
+      options?.afterUpdate?.(newData);
     },
-    setDataInFn: async (fn: (data: T) => T) => {
+    setDataInFn: async (fn: (data: T) => T | Promise<T>) => {
       const nowData = await getData();
 
-      data = fn(nowData);
+      await options?.beforeUpdate?.(nowData);
+      const res = fn(nowData);
+
+      data = isPromise(res) ? await res : res;
+      await options?.afterUpdate?.(data);
     },
   };
 }
